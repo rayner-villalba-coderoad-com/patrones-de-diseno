f534f68bb7daf6f73b7b098b3671d90af0f2567c
rvillalba <rayner.villalba@mojix.com>
2024-03-18 08:03:16 -0400

    Agregando la vista

--- ArquitecturaMVC/src/js/view/todoView.js
+++ ArquitecturaMVC/src/js/view/todoView.js
@@ -1,5 +1,20 @@
 class TodoView {
   constructor() {
     console.log('Todo View!!');
-  }  
+  }
+  
+  // Create an element with an optional CSS class
+  createElement(tag, className) {
+    const element = document.createElement(tag)
+    if (className) element.classList.add(className)
+
+    return element
+  }
+
+  // Retrieve an element from the DOM
+  getElement(selector) {
+    const element = document.querySelector(selector)
+
+    return element
+  }
 }
